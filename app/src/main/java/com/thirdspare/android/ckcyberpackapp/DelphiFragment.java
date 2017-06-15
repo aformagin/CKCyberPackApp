92cec0c3e7ab631aa80486477e0cc8d8c2b7f5ab
auti117 <Austin.Formagin117@gmail.com>
2017-06-15 16:35:26 -0400

    Testing back buttons

--- app/src/main/java/com/thirdspare/android/ckcyberpackapp/DelphiFragment.java
+++ app/src/main/java/com/thirdspare/android/ckcyberpackapp/DelphiFragment.java
@@ -12,17 +12,33 @@ import android.webkit.WebViewClient;
 
 public class DelphiFragment extends Fragment {
 
-    public static DelphiFragment newInstance() {
-        DelphiFragment fragment = new DelphiFragment();
-        return fragment;
-    }
-    private WebView mWebView;
-
+   // public static DelphiFragment newInstance() {
+      //  DelphiFragment fragment = new DelphiFragment();
+      //  return fragment;
+    //}
+    static WebView mWebView;
+    private View mContentView
+private Handler handler = new Handler(){
+        @Override
+        public void handleMessage(Message message) {
+            switch (message.what) {
+                case 1:{
+                    webViewGoBack();
+                }break;
+            }
+        }
+    };
+    
+    
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parentViewGroup, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.frag_delphi, parentViewGroup, false);
-        //WebView mWebView;
+        
+        
+        
+        mContentVIew  = inflater.inflate(R.layout.frag_delphi, null);
+        //inflater.inflate(R.layout.frag_delphi, parentViewGroup, false);
         mWebView = (WebView)view.findViewById(R.id.wbvCD);
+        
         // Enable Javascript
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
@@ -31,21 +47,38 @@ public class DelphiFragment extends Fragment {
 
         // mWebView = (WebView)findViewById(R.id.wbvTest);
         mWebView.setWebViewClient(new WebViewClient());
-        // mWebView.loadUrl("http://www.thirdspare.com");
+       mWebView.loadUrl("http://www.thirdspare.com");
+       mWeb.setOnKeyListener(new OnKeyListener(){
 
-        return view;
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (keyCode == KeyEvent.KEYCODE_BACK 
+                        && event.getAction() == MotionEvent.ACTION_UP 
+                        && mWeb.canGoBack()) {
+                    handler.sendEmptyMessage(1);
+                    return true;
+                }
+
+                return false;
+            }
+
+        });
 
 
 
     }
-    public boolean canGoBack() {
-        return this.mWebView != null && this.mWebView.canGoBack();
+    
+        private void webViewGoBack(){
+        mWeb.goBack();
     }
+    
+  //  public boolean canGoBack() {
+      //  return this.mWebView != null && this.mWebView.canGoBack();
+  //  }
 
-    public void goBack() {
-        if(this.mWebView != null) {
-            this.mWebView.goBack();
-        }
-    }
+   // public void goBack() {
+    //    if(this.mWebView != null) {
+    //        this.mWebView.goBack();
+    //    }
+   // }
 }
 
